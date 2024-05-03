64795dda86e120a1892b5ee6b422eaaf17e02d5f
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-05-03 10:57:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Personal Portfolio
+
+Reference: https://krozo-nextjs-phi.vercel.app/home-light-animation#work
---
+++ index.js
@@ -0,0 +1,12 @@
+if (hold === 'btn') {
+    return scrollTo; 'black';
+}
+function scrollToBlack() {
+    window.scrollTo(black);
+}
+if (hold === 'S-scroll') {
+    return scrollTo; 'yellow white';
+}
+function scrollToYellow() { 
+    window.scrollTo(yellow );
+}   
